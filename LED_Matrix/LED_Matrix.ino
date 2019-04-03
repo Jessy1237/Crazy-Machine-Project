8956fc089e30cfcbc5715ed6af2bea940fc047e5
Jessy1237 <j.e.s.s.e.b.o.yvs@gmail.com>
2019-04-03 18:05:23 +0800

    adding of the extended LedControl Library allowing scrolling text
    includes added features for scrolling text and room for improvement to
    allow for characters that take up less than 6 rows of LEDs. The extended
    alphaBitmap will aid in this endeavour.

---
+++ LED_Matrix/LED_Matrix.ino
@@ -0,0 +1,33 @@
+
+#include "LedExtended.h"
+
+#define DATA_PIN 2
+#define CS_PIN  3
+#define CLK_PIN 4
+#define NBR_MTX 1 //number of matrices attached is one
+
+LedControlExtended lc = LedControlExtended(DATA_PIN, CLK_PIN, CS_PIN, NBR_MTX);
+
+void setup()
+{
+  for (int i = 0; i < NBR_MTX; i++)
+  {
+    lc.shutdown(i, false);
+
+    /* Set the brightness to a medium values */
+    lc.setIntensity(i, 5);
+
+    /* and clear the display */
+    lc.clearDisplay(i);
+    delay(100);
+  }
+}
+
+
+
+void loop()
+{
+  lc.writeScrollingString(0, "Welcome to Super Mario World", 68); //sending characters to display
+  lc.clearAll();//clearing the display
+  delay(1000);
+}
---
+++ LED_Matrix/LedExtended.cpp
@@ -0,0 +1,120 @@
+#include "LedExtended.h"
+#include <string.h>
+/*
+ * LedExtended.cpp - An extended library of LedControlMS for controling Leds with a MAX7219/MAX7221
+ *
+ */
+LedControlExtended::LedControlExtended(int dataPin, int clkPin, int csPin, int numDevices) : LedControl(dataPin, clkPin, csPin, numDevices)
+{
+	
+}
+
+/*
+ * @Author Jesse Sieunarine
+ * @Date 2/04/2019
+ * 
+ * Method for getting the index value of the char in the extended bitmap
+ */
+int LedControlExtended::getCharArrayPositionExtended(char input){
+     if ((input==' ')||(input=='+')) return 10;
+     if (input==':') return 11;
+     if (input=='-') return 12;
+     if (input=='.') return 13;
+     if ((input =='(')) return  14;  //replace by '�'
+     if (input=='!') return 15;	 
+	 if (input=='\'') return 16;
+     if ((input >='0')&&(input <='9')) return (input-'0');
+     if ((input >='A')&&(input <='Z')) return (input-'A' + 17);
+     if ((input >='a')&&(input <='z')) return (input-'a' + 17);     
+     return 13;
+} 
+
+/*
+ * @Author Jesse Sieunarine
+ * @Date 2/04/2019
+ * 
+ * Method for creating a display string for scrolling across the screen. It needs a buffer of 2 spaces at the start and one at the end.
+ */
+char* getDisplayString(char* inputString, int numChar)
+{
+  char* displayString = (char*)malloc(sizeof(char)*(numChar+3));
+  displayString[0] = ' ';
+  displayString[1] = ' ';
+  
+  for(int i=0; i < numChar; i++)
+  {
+    memcpy(&displayString[i+2], &inputString[i], sizeof(char));
+  }
+  
+  displayString[numChar+2] = ' ';
+
+  return displayString;
+}
+
+/*
+ * @Author Jesse Sieunarine
+ * @Date 2/04/2019
+ * 
+ * Method for scrolling text across one display, would be possibly to modify it to work with multiple.
+ * TODO: Modify the method to use the extended bitmap to allow for different sized characters
+ */
+void LedControlExtended::writeScrollingString(int mtx, char* inputString, long delayMS) {
+
+  int numChar = strlen(inputString);
+  char* displayString = getDisplayString(inputString, numChar);
+  
+  int scrollLength = (numChar+2)*6;
+  
+  for(int i=0; i<scrollLength-1; i++)
+  {
+	  int pos;
+	  int nextPos;
+	  int nextNextPos;
+	  char c;
+	  
+	  c = displayString[i/6];
+	  pos = getCharArrayPosition(c);
+	  c = displayString[i/6+1];
+      nextPos = getCharArrayPosition(c);
+	  c = displayString[i/6+2];
+	  nextNextPos = getCharArrayPosition(c);
+
+	  /* Old Code for the Vertical Scrolling
+	  byte currChar[6] = {0x0,0x0,0x0,0x0,0x0,0x0};
+	  byte nextChar[6] = {0x0,0x0,0x0,0x0,0x0,0x0};
+	  memcpy(&currChar, &alphabetBitmap[pos], sizeof(byte)*6);
+	  memcpy(&nextChar, &alphabetBitmap[nextPos], sizeof(byte)*6);
+	  */
+	  
+	  for(int j=0; j<8; j++)
+	  {
+
+		  int shiftLeft =  i % 6;
+		  int index = j + shiftLeft;
+		  
+		  /* Old Code for the Vertical Scrolling
+		  byte data = ((currChar[j] << shiftLeft) | (nextChar[j] >> shiftRight)) & 0xFF;
+		  setRow(mtx, j, data);
+		  */
+		  
+		  if(index < 6)
+		  {
+		    setRow(mtx, j, alphabetBitmap[pos][index]);
+		  }
+		  //Do the next next character
+		  else if ( index - 6 >= 6)
+		  {
+			  setRow(mtx, j, alphabetBitmap[nextNextPos][index-12]);
+		  }
+		  //do the next character
+		  else
+		  {
+			setRow(mtx, j, alphabetBitmap[nextPos][index - 6]);
+		  }
+	  }
+	  
+	  delay(delayMS);
+      clearAll();
+  }
+  free(displayString);
+}
